317d072363b8f6f1ddcaed8e4533ec5facbf0038
UBC Student <nguyenphuocdat0976@gmail.com>
2023-08-11 16:19:18 -0700

    two simple cases

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-#include <cstdint>
 #include "../controller/Task/Task.h"
 #include "../controller/Analysis/Analysis.h"
 
@@ -17,7 +16,6 @@
  * @require:
  */
 
-#include <catch2/catch_test_macros.hpp>
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
@@ -26,11 +24,60 @@ unsigned int Factorial( unsigned int number ) {
 SCENARIO("Success case 1: simple path") {
     GIVEN("initialization: simple path, no changing midway, single end and start") {
         Task taskA = Task(5, 5, "A");
+        std::vector<Task> taskADep;
         Task taskB = Task(10, 10, "B");
+        std::vector<Task> taskBDep;
+        taskBDep.push_back(taskA);
         Task taskC = Task(8, 8, "C");
+        std::vector<Task> taskCDep;
+        taskCDep.push_back(taskA);
         Task taskD = Task(6, 6, "D");
+        std::vector<Task> taskDDep;
+        taskDDep.push_back(taskB);
         Task taskE = Task(7, 7, "E");
+        std::vector<Task> taskEDep;
+        taskEDep.push_back(taskC);
         Task taskF = Task(4, 4, "F");
+        std::vector<Task> taskFDep;
+        taskFDep.push_back(taskC);
         Task taskG = Task(4, 4, "G");
+        std::vector<Task> taskGDep;
+        taskGDep.push_back(taskE);
+        taskGDep.push_back(taskF);
+
+
+
+        std::unordered_map<Task, std::vector<Task>> taskDependencies
+                {
+                        {taskA, taskADep},
+                        {taskB, taskBDep},
+                        {taskC, taskCDep},
+                        {taskD, taskDDep},
+                        {taskE, taskEDep},
+                        {taskG, taskGDep},
+                }
+        ;
+
+
+
+        WHEN("simple path, no changes in critical path midway") {
+            double expected_ECT = 25;
+            std::vector<Task> expected_sorted {taskA, taskB, taskC, taskE, taskF, taskF, taskD, taskG};
+            std::vector<Task> critical_path{taskA, taskB, taskD, taskG};
+        }
+
+        WHEN("simple path, changes in critical path in midway") {
+            taskDependencies.erase(taskE);
+            taskDependencies.erase(taskG);
+            Task taskE = Task(12, 12, "E");
+            Task taskF = Task(10, 10, "F");
+            taskDependencies.insert({taskE, taskEDep});
+            taskDependencies.insert({taskF, taskFDep});
+            double expected_EST = 29;
+            std::vector<Task> expected_sorted {taskA, taskB, taskC, taskE, taskF, taskF, taskD, taskG};
+            std::vector<Task> critical_path{taskA, taskB, taskD, taskG};
+
+        }
+
     }
 }
